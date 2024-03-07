3cf4142f7dec3a6aa15d075aaed1c8c8f3e62bbb
Graham Scott <Grahamlrscott@gmail.com>
2024-03-07 17:42:08 -0600

    Add PlayerPhantom.h

---
+++ src/Spectrum/Player/Base/PlayerPhantom.h
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace app
+{
+namespace Player
+{
+enum ESpectrumPhantomType
+{
+    SPECTRUM_PHANTOM_TYPE_DRILL,
+    SPECTRUM_PHANTOM_TYPE_LASER,
+    SPECTRUM_PHANTOM_TYPE_SPIKE,
+    SPECTRUM_PHANTOM_TYPE_ROCKET,
+    SPECTRUM_PHANTOM_TYPE_FRENZY,
+    SPECTRUM_PHANTOM_TYPE_CUBE,
+    SPECTRUM_PHANTOM_TYPE_HOVER,
+};
+} // Player
+} // app
