439ff5afb85c4f06bc109d14216f0d9ea20c7feb
Graham Scott <Grahamlrscott@gmail.com>
2024-03-07 18:06:34 -0600

    Add WIP player parameters

---
+++ src/Spectrum/Player/Parameter/PlayerParameter.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace app
+{
+namespace Player
+{
+#define SPECTRUM_PLAYER_PARAMETER_0B 150.0f // 0xb
+#define SPECTRUM_PLAYER_PARAMETER_JUMP_BALL_TIMER 0.12f // 0xc
+#define SPECTRUM_PLAYER_PARAMETER_0E 0.29f // 0xe
+#define SPECTRUM_PLAYER_PARAMETER_JUMP_INITIAL 125.4f // 0xf
+#define SPECTRUM_PLAYER_PARAMETER_JUMP_HOLD 327.0f // 0x10
+} // Player
+} // app
